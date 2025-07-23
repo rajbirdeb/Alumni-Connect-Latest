af590563c603d5f7979e0f3be6efc3af252c66a1
Rajbir Deb Mishra <rajbir18mishra@gmail.com>
2025-07-23 22:39:06 +0530

    fixing the cors

--- backend server/server.js
+++ backend server/server.js
@@ -30,8 +30,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "*"
+  origin: "https://alumni-connect-latest.vercel.app",
+  credentials: true
 }));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
